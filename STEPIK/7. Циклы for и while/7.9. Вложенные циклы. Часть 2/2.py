a991d4410c05df096fb48eabfe4a1b1efb6b8ca3
E9uili6rist <70110383+E9uili6rist@users.noreply.github.com>
2020-11-08 17:36:56 +0300

    difficult

---
+++ STEPIK/7. Циклы for и while/7.9. Вложенные циклы. Часть 2/2.py
@@ -0,0 +1,12 @@
+n = 10
+total = 0
+for i in range(n):
+    for j in range(i + 1):
+        total += 1
+        print(total, end="")
+    for j in range(i + 1):
+        total -= 1
+        if total == 0:
+            break
+        print(total, end="")
+    print()
