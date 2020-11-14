a0c62671aa6b0a5723a719b0b168a9a948bceb36
E9uili6rist <70110383+E9uili6rist@users.noreply.github.com>
2020-11-14 21:26:20 +0300

    thanks to the talented Neil's mind and logic

---
+++ STEPIK/0. Skipped tasks/7.3 - 11.py
@@ -0,0 +1,6 @@
+n = 4
+total = 0
+for i in range(1, n+1):
+    if n % i == 0:
+        total += i
+print(total)
---
+++ STEPIK/0. Skipped tasks/7.3 - 12.py
@@ -0,0 +1,6 @@
+n = 3
+total = 0
+for i in range(1, n+1):
+    x = ((-1)**(i+1)) * i
+    total += x
+print(total)
