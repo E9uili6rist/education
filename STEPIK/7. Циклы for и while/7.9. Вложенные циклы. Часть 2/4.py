ad4e45d9957ac1389f8e1d2823fe03fe548b308d
E9uili6rist <70110383+E9uili6rist@users.noreply.github.com>
2020-11-14 23:06:21 +0300

    remember to reset the counter

--- STEPIK/7. Циклы for и while/7.9. Вложенные циклы. Часть 2/4.py
+++ STEPIK/7. Циклы for и while/7.9. Вложенные циклы. Часть 2/4.py
@@ -1,6 +1,9 @@
-n = int(input())
+n = 5
 counter = 0
-for i in range(n):
-    for j in range(i + 1):
-        print(j)
-    print()
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        if i % j == 0:
+            counter += 1
+    print(i, counter * '+', sep='')
+    counter = 0
+print()
