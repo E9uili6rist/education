132c5573c584ef151e4eadfd6950c2b85882ef48
E9uili6rist <70110383+E9uili6rist@users.noreply.github.com>
2020-11-14 21:25:40 +0300

    thanks to the talented Neil's mind and logic

---
+++ STEPIK/0. Skipped tasks/7.3 - 13.py
@@ -0,0 +1,11 @@
+n = 3
+largest1 = -1
+for i in range(n):
+    num = int(input())
+    if num > largest1:
+        largest2 = largest1
+        largest1 = num
+    elif num > largest2:
+        largest2 = num
+print(largest1)
+print(largest2)
